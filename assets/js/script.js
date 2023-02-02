29525a76c51357573bb60e2d1b2818d557aac733
dalto135 <67397294+dalto135@users.noreply.github.com>
2023-02-02 00:21:04 -0500

    gave coordinates to every square

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,16 @@
-function colorCheck(red, green, blue) {
-    let diff = blue;
-    let one = 1 - blue;
-    if (one < diff) {
-        diff = one;
-    }
+let squares = document.querySelector(".row").querySelectorAll("div");
 
-    if (red < (blue - diff) || red > (blue + diff) || green < (blue - diff) || green > (blue + diff)) {
-        return false;
-    }
-    else {
-        return true;
-    }
+function go() {
+    squares.forEach(square => {
+        if (square.getAttribute("id")) {
+            square.removeAttribute("id");
+        }
+        else {
+            square.setAttribute("id", "on");
+        }
+    });
 }
 
-let parent = document.querySelector("html");
-
-function setTheme() {
-    let date = new Date();
-    let time = date.getHours();
-
-    if (time >= 8 && time < 21) {
-        lightMode();
-    }
-    else {
-        darkMode();
-    }
-}
-
-// setTheme();
-
-function grayMode() {
-    parent.setAttribute("id", "gray");
-}
-
-function lightMode() {
-    parent.setAttribute("id", "light");
-}
-
-function darkMode() {
-    parent.setAttribute("id", "dark");
-}
-
-let grayButton = document.querySelector("#grayButton");
-let lightButton = document.querySelector("#lightButton");
-let darkButton = document.querySelector("#darkButton");
-
-grayButton.addEventListener("click", grayMode);
-lightButton.addEventListener("click", lightMode);
-darkButton.addEventListener("click", darkMode);
+// setInterval(() => {
+//     go()
+// }, 250);
