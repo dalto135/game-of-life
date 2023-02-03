756aa878b33f224560f06f251a9ea84e5f1a94fe
dalto135 <67397294+dalto135@users.noreply.github.com>
2023-02-02 23:09:02 -0500

    can now toggle green color for each square with a click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
-let squares = document.querySelectorAll("div");
+let squares = document.querySelector("section").querySelectorAll("button");
 
-function go() {
+function be() {
     squares.forEach(square => {
 
         let location = square.getAttribute("class");
         let locationArray = location.split(',');
         let locationNumberArray = [Number(locationArray[0]), Number(locationArray[1])]
         let adjacent = [];
-        
 
         if (locationNumberArray[0] - 1 >= 0) {
             adjacent.push((locationNumberArray[0] - 1).toString() + ',' + (locationNumberArray[1]).toString());
@@ -15,7 +14,6 @@ function go() {
             if (locationNumberArray[1] - 1 >= 0) {
                 adjacent.push((locationNumberArray[0] - 1).toString() + ',' + (locationNumberArray[1] - 1).toString());
                 adjacent.push((locationNumberArray[0]).toString() + ',' + (locationNumberArray[1] - 1).toString());
-                
             }
             if (locationNumberArray[1] + 1 <= 99) {
                 adjacent.push((locationNumberArray[0] - 1).toString() + ',' + (locationNumberArray[1] + 1).toString());
@@ -27,7 +25,6 @@ function go() {
 
             if (locationNumberArray[1] - 1 >= 0) {
                 adjacent.push((locationNumberArray[0] + 1).toString() + ',' + (locationNumberArray[1] - 1).toString());
-                
             }
             if (locationNumberArray[1] + 1 <= 99) {
                 adjacent.push((locationNumberArray[0] + 1).toString() + ',' + (locationNumberArray[1] + 1).toString());
@@ -38,30 +35,47 @@ function go() {
 
         adjacent.forEach(neighbor => {
             console.log(neighbor);
-            let div = document.querySelector('.' + neighbor);
-            if (div.getAttribute("id")) {
+            let neighborButton = document.querySelector('.' + neighbor);
+            if (neighborButton.getAttribute("id")) {
                 count++;
             }
         })
 
         if (count == 3) {
-            square.setAttribute("id", "on");
+            square.setAttribute("id", "alive");
         }
         else {
             square.removeAttribute("id");
         }
 
-        if (square = squares[150]) {
-            console.log('LOCATION');
-            console.log(location);
-            console.log('ADJACENT');
-            console.log(adjacent);
-        }
+        // if (square = squares[150]) {
+        //     console.log('LOCATION');
+        //     console.log(location);
+        //     console.log('ADJACENT');
+        //     console.log(adjacent);
+        // }
     });
 }
 
-// setInterval(() => {
-//     go()
-// }, 250);
+squares.forEach(square => {
+    function cross() {
+        if (square.getAttribute("id")) {
+            square.removeAttribute("id");
+        }
+        else {
+            square.setAttribute("id", "alive");
+        }
+    }
 
-go();
+    square.addEventListener("click", cross);
+})
+
+let button = document.querySelector("#be");
+
+function start() {
+    setInterval(() => {
+        be();
+    }, 250);
+}
+
+button.addEventListener("click", start);
